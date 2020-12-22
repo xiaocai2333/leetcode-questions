3815b1042d8d3ca4b737824ffe451cc251a029c2
usrname <email>
2020-12-22 19:33:59 +0800

    add zigzagLevelOrder

--- zigzagLevelOrder/zigzagLevelOrder.go
+++ zigzagLevelOrder/zigzagLevelOrder.go
@@ -11,6 +11,8 @@
 
 package main
 
+import "fmt"
+
 /**
 * Definition for a binary tree node.
 */
@@ -21,6 +23,86 @@ type TreeNode struct {
  }
 
 func zigzagLevelOrder(root *TreeNode) [][]int {
+	isEnd := false
+	var result [][]int
+	h := 1
+	var roots []*TreeNode
+	i := 0
+	Loop:
+		for {
+			var preLayerVal []int
+			if root == nil {
+				isEnd = true
+				break Loop
+			}
+			isEnd = true
+			n := len(roots)
+			if h == 1 {
+				preLayerVal = append(preLayerVal, root.Val)
+				roots = append(roots, root)
+				isEnd = false
+			} else {
+				for j := n - 1; j >= i; j-- {
+					root := roots[j]
+					if h % 2 == 0 {
+						if root.Right != nil {
+							preLayerVal = append(preLayerVal, root.Right.Val)
+							roots = append(roots, root.Right)
+							isEnd = false
+						}
+						if root.Left != nil {
+							preLayerVal = append(preLayerVal, root.Left.Val)
+							roots = append(roots, root.Left)
+							isEnd = false
+						}
+					} else {
+						if root.Left != nil {
+							preLayerVal = append(preLayerVal, root.Left.Val)
+							roots = append(roots, root.Left)
+							isEnd = false
+						}
+						if root.Right != nil {
+							preLayerVal = append(preLayerVal, root.Right.Val)
+							roots = append(roots, root.Right)
+							isEnd = false
+						}
+					}
+				}
+			}
+			if isEnd == true {
+				break Loop
+			}
+			h++
+			i = n
+			result = append(result, preLayerVal)
 
-	return nil
+		}
+	return result
 }
+
+func main() {
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+			Left: nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val: 20,
+			Left: &TreeNode{
+				Val: 15,
+				Left: nil,
+				Right: nil,
+			},
+			Right: &TreeNode{
+				Val: 7,
+				Left: nil,
+				Right: nil,
+			},
+		},
+	}
+
+	fmt.Println(zigzagLevelOrder(root))
+}
+
